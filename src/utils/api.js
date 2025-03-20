39e9bcf14495cd95d215ee9a2c336ea6faf26e13
hmgny <hmerveguney@hotmail.com>
2025-03-21 00:59:56 +0300

    api.js oluşturuldu

---
+++ src/utils/api.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000';
+
+export const login = async (credentials) => {
+    const { username, password } = credentials;
+
+    try {
+        const response = await axios.post(`${API_URL}/user/login`, { username, password });
+        return response;
+    } catch (error) {
+        console.error("Login API Error:", error);
+        throw error;
+    }
+};
+
+
+export const register = async (userData) => {
+    return axios.post(`${API_URL}/register`, userData);
+};
+
+export const postTweet = async (tweet) => {
+    const token = localStorage.getItem('authToken');
+    return await axios.post('http://localhost:3000/tweets/user/{userId}', tweet, {
+        headers: {
+            Authorization: `Basic ${token}`
+        }
+    });
+};
+
+export const fetchTweets = async () => {
+    const token = localStorage.getItem('authToken');
+
+    if (!token) {
+        throw new Error("No auth token found");
+    }
+
+    return axios.get(`http://localhost:3000/tweet`, {
+        headers: {
+            'Authorization': `Basic ${token}` 
+        }
+    });
+};
+
+export const getAllTweets = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/tweet`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const createTweet = async (userId, tweetText) => {
+    try {
+        const response = await axios.post(`${API_URL}/tweet/user`, {
+            userId: userId,
+            tweetText: tweetText,
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const deleteTweet = async (tweetId) => {
+    try {
+        await axios.delete(`${API_URL}/tweet/user/${tweetId}`);
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const updateTweet = async (tweetId, tweetText) => {
+    try {
+        await axios.put(`${API_URL}/tweet/user/${tweetId}`, {
+            tweetText: tweetText,
+        });
+    } catch (error) {
+        throw error;
+    }
+};
