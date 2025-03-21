479b4bfbebb0c387b202c3ec0c6505c6dd20f151
hmgny <hmerveguney@hotmail.com>
2025-03-22 01:17:10 +0300

    Homepage e tweet form eklendi

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,15 @@
+import TweetForm from "../components/TweetForm";
 import TweetList from "../components/TweetList";
 import Layout from "../layout/Layout";
 
 const HomePage = () => {
+  const handleNewTweet = (newTweet) => {
+    // Handle the new tweet
+  };
+
   return (
     <Layout>
+      <TweetForm onTweetCreated={handleNewTweet} />
       <TweetList />
     </Layout>
   );
